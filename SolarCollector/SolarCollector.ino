b5e63282d5ddbc9c685d05b9b1a0b8b60eb44b7c
Chris Woerz <erendrake@gmail.com>
2015-11-29 22:06:59 -0700

    finally compiling again

--- SolarCollector/SolarCollector.ino
+++ SolarCollector/SolarCollector.ino
@@ -1,56 +1,129 @@
 #include "config.h"
+#include <MySensor.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int panelPumpLedPin = PANEL_PUMP_LED_PIN;
-const int panelPumpPin = PANEL_PUMP_PIN;
-const int tempBusPin = TEMP_BUS_PIN;
-const int tempPollingDelay = TEMP_POLLING_DELAY;
+// Communications
 const int serialBPS = SERIAL_BPS;
-const int runtimePin = RUNTIME_PIN;
+
+// Timer
+const int minimumPumpOnSeconds = MINIMUM_PUMP_ON_SECONDS;
+const int tempPollingDelay = TEMP_POLLING_DELAY_SECONDS;
+
+// LED Pins
+const int panelPumpLedPin = PANEL_PUMP_LED_PIN;
+const int runtimePin = RUNTIME_LED_PIN;
+const int generalAlarmPin = GENERAL_ALARM_LED_PIN;
+
+// Relay Pins
+const int panelPumpPin = PANEL_PUMP_PIN;
+
+// Sensor Pins
+const int tempBusPin = TEMP_BUS_PIN;
+
+// Sensor Settings
+const int tempResolution = TEMP_9_BIT;
+const DeviceAddress tempPanelSensorAddress = TEMP_PANEL_SENSOR_ADDRESS;
+const DeviceAddress tempTankSensorAddress = TEMP_TANK_SENSOR_ADDRESS;
+
 
 OneWire  ds(tempBusPin);
 DallasTemperature sensors(&ds);
-  
+
+
+ 
 void setup() {
-  pinMode(panelPumpLedPin, OUTPUT);  
-  pinMode(runtimePin, OUTPUT);  
-  digitalWrite(runtimePin, LOW);
+  setupLedPins();
+  
   Serial.begin(serialBPS);
-  Serial.println("Kelvinated Online Occular Tempcontrol");
-  sensors.begin();
+
+  setupTempSensors();
 }
 
 void loop() {
   digitalWrite(runtimePin, LOW);
   requestTempSensors();
   logTempSensors();  
+
+  processPanelPump();
+  
   digitalWrite(runtimePin, HIGH);  
-  delay(tempPollingDelay);
+  delay(tempPollingDelay * 1000);
+}
+
+
+  
+
+
+
+void setupTempSensors(){  
+  Serial.println("Solaranator 4000");
+  
+  sensors.begin();  
+  sensors.setResolution(tempResolution);
+  int deviceCount = sensors.getDeviceCount();
+  Serial.print("Found ");
+  Serial.print(deviceCount);
+  Serial.println(" temp probes");   
+
+  for (int i = 0; i < deviceCount; i++) {
+    DeviceAddress address;
+    if(sensors.getAddress(address, i))
+    {
+      Serial.print("Address for the device ");
+      Serial.print(i +1);
+      Serial.print(" (index ");    
+      Serial.print(i);
+      Serial.print(" ) is: ");
+      printAddress(address);        
+      Serial.println("");
+    } 
+  }
+}
+
+void processPanelPump(){
+  
+  
+}
+
+void setupLedPins(){
+  pinMode(panelPumpLedPin, OUTPUT);   
+  digitalWrite(panelPumpLedPin, HIGH);  
+  pinMode(panelPumpPin, OUTPUT);   
+  digitalWrite(panelPumpPin, HIGH);  
+  pinMode(panelPumpLedPin, OUTPUT);  
+  digitalWrite(panelPumpLedPin, HIGH);  
+  pinMode(runtimePin, OUTPUT);  
+  digitalWrite(runtimePin, HIGH);  
 }
 
 void requestTempSensors(){  
   Serial.print("Requesting temperatures from pin: ");
-  Serial.print(tempBusPin);
-  Serial.print(" ");  
+  Serial.println(tempBusPin);
   
   sensors.requestTemperatures(); // Send the command to get temperatures
   
-  Serial.print("DONE: Found ");
-  Serial.print(sensors.getDeviceCount());
-  Serial.println(" temp probes");   
 }
 
 void logTempSensors(){
   // After we got the temperatures, we can print them here.
   // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  for (int i = 0; i < sensors.getDeviceCount(); i++) {
+  for (int i = 0; i < sensors.getDeviceCount(); i++) { 
     Serial.print("Temperature for the device ");
-    Serial.print(i +1);
-    Serial.print(" (index 0) is: ");
+    Serial.print(i + 1);
+    Serial.print(" ( index: ");    
     Serial.print(i);
-    Serial.print(") is: ");
-    Serial.println(sensors.getTempCByIndex(i));  
+    Serial.print(" ) is: ");
+    Serial.println(sensors.getTempFByIndex(i));  
   }
 }
 
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
--- SolarCollector/config.h
+++ SolarCollector/config.h
@@ -1,26 +1,28 @@
 //Communications
-//#define SERIAL_BPS 230400
-//#define SERIAL_BPS 115200
 #define SERIAL_BPS 19200
 
 //Temperature Thresholds
-#define SYSTEM_DIFF_ON 18
+#define SYSTEM_DIFF_ON 10
 #define SYSTEM_DIFF_OFF -5
 #define SYSTEM_OVERHEAT 160
 
 //Panel Pump
 #define PANEL_PUMP_LED_PIN 7
 #define PANEL_PUMP_PIN 4
+#define MINIMUM_PUMP_ON_SECONDS 120
 
 //Heater Pump
 #define HEATER_PUMP_LED_PIN 8
 #define HEATER_PUMP_PIN 5
 
 //Diagnostic LED
-#define GENERAL_ALARM_PIN 9
-#define RUNTIME_PIN 6
+#define GENERAL_ALARM_LED_PIN 9
+#define RUNTIME_LED_PIN 6
 
 //Temp Sensors
 #define TEMP_BUS_PIN 3
-#define TEMP_POLLING_DELAY 3000
+#define TEMP_POLLING_DELAY_SECONDS 10
+#define TEMP_PANEL_SENSOR_ADDRESS { 0x28, 0xB6, 0xE6, 0x1D, 0x07, 0x00, 0x00, 0x32 }
+#define TEMP_TANK_SENSOR_ADDRESS { 0x28, 0xAE, 0x9F, 0x2A, 0x07, 0x00, 0x00, 0x98 }
+
 
