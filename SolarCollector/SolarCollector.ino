b96c415b1612cbd659a7bf7b401f9e5cfc63ccba
Chris Woerz <erendrake@gmail.com>
2015-12-05 01:32:01 -0700

    logging and basic pump control

--- SolarCollector/SolarCollector.ino
+++ SolarCollector/SolarCollector.ino
@@ -1,10 +1,14 @@
 #include "config.h"
-#include <MySensor.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 // Communications
 const int serialBPS = SERIAL_BPS;
+
+//Temperature Thresholds
+const int systemDiffOn = SYSTEM_DIFF_ON;
+const int systemDiffOff = SYSTEM_DIFF_OFF;
+const int systemOverheat = SYSTEM_OVERHEAT;
 
 // Timer
 const int minimumPumpOnSeconds = MINIMUM_PUMP_ON_SECONDS;
@@ -28,24 +32,29 @@ const char* tempPanelSensorName = TEMP_PANEL_SENSOR_NAME;
 const DeviceAddress tempTankSensorAddress = TEMP_TANK_SENSOR_ADDRESS;
 const char* tempTankSensorName = TEMP_TANK_SENSOR_NAME;
 
-
 OneWire  ds(tempBusPin);
 DallasTemperature sensors(&ds);
 
+// Current Readings
+float currentTankTemp = -127.0;
+float currentPanelTemp = -127.0;
+
+// Pump Status
+bool panelPumpStatus = false;
 
  
 void setup() {
-  setupLedPins();
-  
+  setupLedPins(); 
+  pinMode(panelPumpPin, OUTPUT); 
   Serial.begin(serialBPS);
-
   setupTempSensors();
 }
 
 void loop() {
   digitalWrite(runtimePin, LOW);
   requestTempSensors();
-  logTempSensors();  
+  currentPanelTemp = logTempSensor(tempPanelSensorAddress, tempPanelSensorName);  
+  currentTankTemp = logTempSensor(tempTankSensorAddress, tempTankSensorName);  
 
   processPanelPump();
   
@@ -66,31 +75,56 @@ void setupTempSensors(){
   int deviceCount = sensors.getDeviceCount();
   Serial.print("Found ");
   Serial.print(deviceCount);
-  Serial.println(" temp probes");   
+  Serial.println(" temp probes");     
+  ds.reset_search();
 
-  
-  logSensorFound(tempPanelSensorAddress, tempPanelSensorName);
-  logSensorFound(tempTankSensorAddress, tempTankSensorName);
+  bool foundPanelSensor = false;
+  bool foundTankSensor = false;
+
+  byte addr[8];
+  while(ds.search(addr)){
+     if(memcmp(tempPanelSensorAddress, addr, 8) == 0)
+     {
+       Serial.println("Found Panel!");
+       foundPanelSensor = true;
+     }
+     else if(memcmp(tempTankSensorAddress, addr, 8) == 0)
+     {
+       Serial.println("Found Tank!");
+       foundTankSensor = true;
+     }
+     else {
+       Serial.println("Found Other!");
+     }
+  }
+
+  if(foundPanelSensor && foundTankSensor){
+    Serial.println("Woot!");
+  }
+  else {
+    Serial.println("Bummer!");
+  }
 }
 
-void logSensorFound(const DeviceAddress addr, const char* sensorName){
-  Serial.print(sensorName);
-  Serial.print(" sensor ");
-  DeviceAddress blarg;
-  memcpy(blarg, addr, sizeof(DeviceAddress)); 
-  if(!ds.search(blarg)){
-    Serial.print("NOT ");
-  }  
-  
-  Serial.print("found at address ");
-  printAddress(blarg);
-  Serial.println("");
-  
-}
 
 void processPanelPump(){
-  
-  
+  float systemDifference = currentPanelTemp - currentTankTemp;
+
+  if (currentTankTemp > systemOverheat){  
+    digitalWrite(panelPumpPin, LOW);  
+    return;
+  }
+
+  if (systemDifference > systemDiffOn && panelPumpStatus == false){
+    digitalWrite(panelPumpPin, HIGH);  
+    Serial.println("PanelPump: On!");
+    panelPumpStatus = true;
+  }
+  else if(systemDifference < systemDiffOff && panelPumpStatus == true){
+    digitalWrite(panelPumpPin, LOW);  
+    Serial.println("PanelPump: Off!");
+    panelPumpStatus = false;
+  }
 }
 
 void setupLedPins(){
@@ -112,18 +146,13 @@ void requestTempSensors(){
   
 }
 
-void logTempSensors(){
-  // After we got the temperatures, we can print them here.
-  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
-  for (int i = 0; i < sensors.getDeviceCount(); i++) { 
-    
+float logTempSensor(const DeviceAddress address, const char* name){    
     Serial.print("Temperature for the device ");
-    Serial.print(i + 1);
-    Serial.print(" ( index: ");    
-    Serial.print(i);
-    Serial.print(" ) is: ");
-    Serial.println(sensors.getTempFByIndex(i));  
-  }
+    Serial.print(name);
+    Serial.print(" is: ");
+    float result = sensors.getTempF(address);
+    Serial.println(result);  
+    return result;
 }
 
 // function to print a device address
