314f29107dfa466d1900ef34a2218b59f4efeed8
Richard Woerz <rwoerz@gmail.com>
2016-10-21 17:29:59 -0600

    spike new sketch for attic fan

---
+++ AtticFan/config.h
@@ -0,0 +1,25 @@
+//Communications
+#define SERIAL_BPS 115200
+
+//Temperature Thresholds
+#define SYSTEM_DIFF_ON 10
+#define SYSTEM_DIFF_OFF -5
+#define SYSTEM_OVERHEAT 160
+
+//Panel Pump
+#define PANEL_PUMP_PIN 4
+#define MINIMUM_PUMP_ON_SECONDS 120
+
+//Heater Pump
+#define HEATER_PUMP_PIN 5
+
+//Diagnostic LED
+#define RUNTIME_LED_PIN 6
+#define GENERAL_ALARM_LED_PIN 9
+
+//Temp Sensors
+#define TEMP_BUS_PIN 3
+#define TEMP_POLLING_DELAY_SECONDS 3
+#define TEMP_PANEL_SENSOR_NAME "Panel Sensor"
+#define TEMP_TANK_SENSOR_NAME "Tank Sensor"
+
